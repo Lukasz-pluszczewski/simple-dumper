ebe7eae0b06f99578dd0658c2bc661f08b915bb0
lukasz_brainhub <lukasz@brainhub.pl>
2019-01-24 21:37:09 +0100

    Added help route

--- src/v1/index.js
+++ src/v1/index.js
@@ -50,7 +50,7 @@ const filterMiddleware = (req, count = false) => wrapper => {
   if (count) {
     return wrapper.size();
   }
-  return wrapper;
+  return wrapper.reverse();
 };
 
 export default () => {
@@ -72,6 +72,41 @@ export default () => {
   api.delete('/requests/:id', (req, res) => {
     resourceCrud.delete('requests', req.params.id);
     res.json({ message: 'ok' });
+  });
+
+  api.get('/help', (req, res) => {
+    res.send(`<article>
+    <h1><a href="https://github.com/Lukasz-pluszczewski/simple-dumper">Request dumper</a></h1>
+      <blockquote>
+        <p>> Node app that saves all incoming requests and allows you to search through history</p>
+      </blockquote>
+      <h2>Endpoints</h2>
+      <h4>GET /requests</h4>
+      <p>Returns the array of saved requests (with simplified data)</p>
+      <p>Params:</p>
+      <ul>
+        <li>full - if set to 'true' returns all saved fields</li>
+        <li>body, query, headers, baseUrl, method, date: regexes to return only entries containing particular string</li>
+      </ul>
+      <p>Examples:</p>
+      <p>
+        <code>/requests?full=true&amp;baseUrl=^test&amp;method=POST</code> - will return full information about requests whose baseUrl starts with 'test' and method was POST
+      </p>
+      <p>
+        <code>/requests?body=test</code> - will return requests that contain string 'test' in body (in key or in value)
+      </p>
+      <h4>GET /count</h4>
+      <p>Identical to /requests but returns number of entries</p>
+      <p>Accepts the same params</p>
+      <h4>DELETE /requests</h4>
+      <p>Clears the database</p>
+      <h4>GET /requests/:id</h4>
+      <p>Gets full information about request by it's id</p>
+      <h4>DELETE /requests/:id</h4>
+      <p>Deletes one entry</p>
+      <h2>Docker</h2>
+      <p><code>docker run -d sigmification/simple-dumper</code></p>
+</article>`);
   });
   api.use('*', (req, res) => {
     const requestData = {
